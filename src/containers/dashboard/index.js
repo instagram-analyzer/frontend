1dde67cc022d16e946421bbd74858d541b94c846
KieranVieira <kieranvieira@live.com>
2019-05-12 01:26:35 -0400

    did some stuff

--- src/containers/dashboard/index.js
+++ src/containers/dashboard/index.js
@@ -4,12 +4,14 @@ import authHOC from '../../components/auth-hoc';
 
 import NavBar from '../../components/navigation';
 import Instagram from './instagram';
+import InstagramSearch from './instagram/instagram-search';
 
 export class Dashboard extends React.Component {
   render() {
     return (
       <>
         <Route path="/dashboard" component={NavBar}/>
+        <Route exact path="/dashboard" component={InstagramSearch}/>
         <Route path="/dashboard/instagram" component={Instagram} />
       </>
     )
